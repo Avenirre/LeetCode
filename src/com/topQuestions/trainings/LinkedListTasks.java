5480cd334b21c657234701450092bb8e4c9138c9
RomanVinokurov <avenirre@gmail.com>
2024-04-02 15:11:33 +0300

    Finish LL

--- src/com/topQuestions/trainings/LinkedListTasks.java
+++ src/com/topQuestions/trainings/LinkedListTasks.java
@@ -18,30 +18,34 @@ public class LinkedListTasks {
 
 
     public static void main(String[] args) {
-        LinkedNode head1 = new LinkedNode(0);
+        LinkedNode head1 = new LinkedNode(8);
         head1.next = new LinkedNode(2);
-        head1.next.next = new LinkedNode(5);
-        head1.next.next.next = new LinkedNode(8);
+        head1.next.next = new LinkedNode(2);
+        head1.next.next.next = new LinkedNode(3);
 
         LinkedNode head2 = new LinkedNode(1);
         head2.next = new LinkedNode(2);
         head2.next.next = new LinkedNode(7);
         //mergeTwoSortedLists(head1, head2);
         //getMiddleOfLinkedList(head1);
-        reverseLinkedList(head1);
+        //reverseLinkedList(head1);
+        //palindromeLinkedList(head1);
+        //linkedListCycle(head1);
+        removeDuplicatesFromLinkedList(head1);
     }
 
-    private static void reverseLinkedList(LinkedNode head) {
+    private static LinkedNode removeDuplicatesFromLinkedList(LinkedNode head) {
         LinkedNode curr = head;
-        LinkedNode prev = null;
+
         while (curr != null) {
-            LinkedNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
         }
 
-        printLL(prev);
+        return head;
     }
 
 
@@ -84,6 +88,80 @@ public class LinkedListTasks {
         }
         System.out.println(slow.val);
         return slow.val;
+    }
+
+    private static void reverseLinkedList(LinkedNode head) {
+        LinkedNode curr = head;
+        LinkedNode prev = null;
+        while (curr != null) {
+            LinkedNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        printLL(prev);
+    }
+
+    private static boolean palindromeLinkedList(LinkedNode head) {
+        LinkedNode secondHalfStart = findSecondHalfBeginning(head);
+        LinkedNode reversedSecondHalfStart = reverseLL(secondHalfStart);
+
+        while (head != null && reversedSecondHalfStart != null) {
+            if (head.val != reversedSecondHalfStart.val) {
+                System.out.println("False");
+                return false;
+            }
+            head = head.next;
+            reversedSecondHalfStart = reversedSecondHalfStart.next;
+
+        }
+
+        System.out.println("True");
+        return true;
+    }
+
+    private static LinkedNode reverseLL(LinkedNode head) {
+        LinkedNode prev = null;
+        LinkedNode curr = head;
+
+        while (curr != null) {
+            LinkedNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    private static LinkedNode findSecondHalfBeginning(LinkedNode head) {
+        LinkedNode slow = head;
+        LinkedNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private static boolean linkedListCycle(LinkedNode head) {
+        LinkedNode slow = head;
+        LinkedNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                System.out.println("False");
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println("True");
+        return true;
     }
 
     public static void printLL(LinkedNode curr) {
